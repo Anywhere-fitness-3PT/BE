dff1686562ae6b723004a2160126bfd2edf62cba
Tasha <tashingsworth@gmail.com>
2020-09-17 19:18:34 -0400

    created user roles table

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,13 @@ require('dotenv').config()
 module.exports = {
     development: {
         client: "pg",
-        useNullAsDefault: true,
-        connection: process.env.DB_URL,
+        // useNullAsDefault: true,
+        connection: {
+            host : process.env.DB_HOST,
+            user : process.env.DB_USER,
+            password : process.env.DB_PASSWORD,
+            database : process.env.DB_DATABASE,
+        },
         migrations: {
             directory: "./database/migrations",
         },
