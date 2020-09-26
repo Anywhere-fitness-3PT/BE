606198be5ae2bd5b756d882cc8bb7ef4ca7308a7
Evelyn Nguyen <evelynnguyen@evelyns-mbp.lan>
2020-09-25 18:15:27 -0700

    two tests

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,12 @@ module.exports = {
     },
     test: {
         client: "pg",
-        connection: process.env.DB_URL,
+        connection: {
+            host : process.env.DB_HOST,
+            user : process.env.DB_USER,
+            password : process.env.DB_PASSWORD,
+            database : process.env.DB_DATABASE,
+        },
         migrations: {
             directory: "./database/migrations",
         },
